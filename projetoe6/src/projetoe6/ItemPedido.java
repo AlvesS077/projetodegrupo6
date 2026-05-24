ff99c5986b3aac66444f65ac0669222151058901
ASUS <ASUS@192.168.1.250>
2026-05-24 17:54:51 +0100

    Classe feita

--- projetoe6/src/projetoe6/ItemPedido.java
+++ projetoe6/src/projetoe6/ItemPedido.java
@@ -1,37 +1,60 @@
 package projetoe6;
 
 public class ItemPedido {
-	private Produto produto;
+
+    private Produto produto;
     private int quantidade;
     private String notas;
- 
-    public ItemPedido(Produto produto, int quantidade, String notas) {
+
+    public ItemPedido(Produto produto, int quantidade) {
         this.produto = produto;
         this.quantidade = quantidade;
-        this.notas = notas;
+        this.notas = "";
     }
- 
+
+    public ItemPedido(Produto produto, int quantidade, String notas) {
+        this(produto, quantidade);
+        if (notas != null) {
+            this.notas = notas;
+        } else {
+            this.notas = ""; // Se for nulo, fica vazio por segurança
+        }
+    }
+    
     public Produto getProduto() {
         return produto;
     }
- 
+
     public int getQuantidade() {
         return quantidade;
     }
- 
+
     public String getNotas() {
         return notas;
     }
- 
-    public void setNotas(String n) { /* pode ser corrigida/atualizada */
-        this.notas = n;
-    }
- 
-  
-    public String toString() {
-        return "ItemPedido{produto=" + produto.getNome() +
-               ", quantidade=" + quantidade + ", notas='" + notas + "'}";
-    }
-}
- 
 
+    public void setNotas(String n) {
+        if (n != null) {
+            this.notas = n;  
+        } else {
+            this.notas = ""; 
+        }
+    }
+
+    public double getSubtotal() {
+        return produto.getPreco() * quantidade;
+    }
+
+    @Override
+    public String toString() {
+        
+        String linha = "  " + quantidade + "x " + produto.getNome() + " = " + getSubtotal() + "€";
+        
+        
+        if (!notas.isEmpty()) {
+            linha = linha + " [nota: " + notas + "]";
+        }
+        
+        return linha;
+    }
+}	
